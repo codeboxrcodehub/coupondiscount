aa74efdbc32037bfd652a7e6d86dd3a9bcf13b67
Dipu Chandra Dey <dipudey764@gmail.com>
2022-12-23 11:39:20 +0600

    update migration

--- src/CouponDiscountServiceProvider.php
+++ src/CouponDiscountServiceProvider.php
@@ -14,9 +14,11 @@ class CouponDiscountServiceProvider extends ServiceProvider
      */
     public function boot()
     {
+        $time = time();
+
         $this->publishes([
-            __DIR__ . '/../database/migrations/create_coupons_table.stub'          => database_path('migrations/' . date('Y_m_d_His', time()) . '_create_coupons_table.php'),
-            __DIR__ . '/../database/migrations/create_coupon_histories_table.stub' => database_path('migrations/' . date('Y_m_d_His', time()) . '_create_coupon_histories_table.php'),
+            __DIR__ . '/../database/migrations/create_coupons_table.stub'          => database_path('migrations/' . date('Y_m_d_His', $time) . '_create_coupons_table.php'),
+            __DIR__ . '/../database/migrations/create_coupon_histories_table.stub' => database_path('migrations/' . date('Y_m_d_His', $time + 1) . '_create_coupon_histories_table.php'),
         ], 'migrations');
     }
 
